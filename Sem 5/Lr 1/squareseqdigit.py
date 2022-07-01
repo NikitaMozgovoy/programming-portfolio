4b9072f32d0cb96b4644e34c8a2fe007c0a794fd
mozgovoy <60607081+mozgovoy@users.noreply.github.com>
2022-07-01 16:22:49 +0300

    Add files via upload

--- Sem 5/Lr 1/squareseqdigit.py
+++ Sem 5/Lr 1/squareseqdigit.py
@@ -1,1 +1,19 @@
+import math
 
+def squareSequenceDigit(n):
+    x = 1
+    while(True):
+        s = str(x**2)
+        if (len(s) < n):
+            n -= len(s)
+        else:
+            return int(s[n-1])
+        x += 1
+
+if __name__ == "__main__":
+    print(squareSequenceDigit(1))
+    print(squareSequenceDigit(2))
+    print(squareSequenceDigit(7))
+    print(squareSequenceDigit(12))
+    print(squareSequenceDigit(17))
+    print(squareSequenceDigit(27))
